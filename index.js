46fbeacf724c86f14e1a31fdee94626b7c1a55bd
SYLVESTER OBED JAMES <84601547+sojames@users.noreply.github.com>
2026-09-21 06:11:30 +0000

    commented 3.2

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const port = process.env.PORT||3345;
 const AppName = process.env.APP_NAME
 // const AppName = process.env.APP_NAME
 // const AppName = process.env.APP_NAME
-// const AppName = process.env.APP_NAME
-// const AppName = process.env.APP_NAME
 
 const middleware = (req, res, next) => {
     const key = req.query['key']|| req.headers['key'];
