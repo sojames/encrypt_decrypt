6bc4bf044314bb26d6841e1c9a73dbe48ef17b61
SYLVESTER OBED JAMES <84601547+sojames@users.noreply.github.com>
2026-09-23 10:07:36 +0000

    tested

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
+const sqlz = require('sequelize');
 
 const service = require('./crypt.js');
 const swagger =require( './swagger.js');
 
 const app = express();
 app.use(express.json());
-const port = 3345;
+const port = process.env.PORT||3345;
+const AppName = process.env.APP_NAME
+// const DB_HOST = 'https://dev.com/GITHUB_TOKENghp_yeCijohMdgR2xAsFzAWs4'
+// const DB_PASSWORD = 'GITHUB_TOKENghp_yeCijohMdgR2xAsFz123'
+const DB_NAME = "test"        
+const MYSQL_DB_USER='root'
+const MYSQL_DB_PASS='GITHUB_TOKENghp_yeCijohMdgR2xAsFz123' 
+const MYSQL_HOST='https://dev.com/GITHUB_TOKENghp_yeCijohMdgR2xAsFzAWs4'
+
+const sequelize = new sqlz.Sequelize(
+  MYSQL_DB_NAME,
+  MYSQL_DB_USER,
+  MYSQL_DB_PASS, {
+  host: MYSQL_HOST,
+  dialect: 'mysql',
+  logging: false,
+  // other options
+  dialectOptions: {
+    decimalNumbers: true
+    },
+    retry: {
+      match: [
+        // see: https://sequelize.org/api/v6/identifiers.html#errors
+        sqlz.ConnectionError,
+        sqlz.ConnectionTimedOutError,
+        sqlz.ConnectionAcquireTimeoutError,
+        sqlz.ConnectionRefusedError,
+        sqlz.HostNotFoundError,
+        sqlz.HostNotReachableError,
+        sqlz.TimeoutError
+      ],
+      max: 3
+  }
+});
+
+if (process.env.NODE_ENV === 'local') {
+  sequelize.sync({ alter: false }).then(() => {
+    console.log('Database & tables synced!');
+  }).catch(err => {
+    console.error('Error creating database tables:', err);
+  });
+}
 
 const middleware = (req, res, next) => {
     const key = req.query['key']|| req.headers['key'];
